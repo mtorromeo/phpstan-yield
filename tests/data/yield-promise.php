491ec0fc5a17c8c33a5b1b16b8cc15d1944a123d
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2026-03-24 00:04:03 +0100

    test: added asserts for testing yield type resolution involving class inheritance

--- tests/data/yield-promise.php
+++ tests/data/yield-promise.php
@@ -34,9 +34,47 @@ function fetchGenericCall($param): TemplatedPromiseLike
     return new TemplatedPromiseLike();
 }
 
+/**
+ * @template T
+ * @yield T
+ */
+interface PromiseInterface {}
+
+/**
+ * @template T
+ * @implements PromiseInterface<T>
+ */
+class ConcretePromise implements PromiseInterface {}
+
+/**
+ * @template T
+ * @yield T
+ */
+class BasePromise {}
+
+/**
+ * @template T
+ * @extends BasePromise<T>
+ */
+class ChildPromise extends BasePromise {}
+
+/** @return ConcretePromise<int> */
+function fetchIntFromInterface(): ConcretePromise
+{
+    return new ConcretePromise();
+}
+
+/** @return ChildPromise<bool> */
+function fetchBoolFromChild(): ChildPromise
+{
+    return new ChildPromise();
+}
+
 function yieldPromiseLike(): Generator
 {
     assertType('string', yield fetchString());
     assertType('array{int, non-empty-string}', yield fetchComplexArray());
     assertType('float', yield fetchGenericCall(1.24));
+    assertType('int', yield fetchIntFromInterface());
+    assertType('bool', yield fetchBoolFromChild());
 }
